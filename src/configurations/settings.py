d21c767a90d0694dceef2ad2d178de54d42360c3
sanonesan <ezcudze@gmail.com>
2024-03-03 13:33:52 +0300

    refactoring

--- src/configurations/settings.py
+++ src/configurations/settings.py
@@ -28,8 +28,8 @@ class AuthJWT(BaseModel):
 
 class Settings(BaseSettings):
     # for PostgreSQL
-    db_host: str = "postgresql+asyncpg://postgres:postgres@127.0.0.1:5445"
-    db_name: str = "fastapi_project_db"
+    db_host: str
+    db_name: str
     db_test_name: str = "fastapi_project_test_db"
     max_connection_count: int = 10
 
@@ -43,7 +43,7 @@ class Settings(BaseSettings):
     def database_test_url(self) -> str:
         return f"{self.db_host}/{self.db_test_name}"
 
-    model_config = SettingsConfigDict(env_file=".env.example", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
 settings = Settings()
